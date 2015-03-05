0f66d39dc3eb8a106257c155aac018855aad2701
NicolajPedersen <nicolajgpedersen@gmail.com>
2015-03-05 11:39:29 +0100

    Databasefacade + getting all products

--- JamFactory/Controller/Database/OptimeringDatabaseFacade.cs
+++ JamFactory/Controller/Database/OptimeringDatabaseFacade.cs
@@ -12,18 +12,37 @@ namespace JamFactory.Controller.Database {
         private static SqlConnection dbconn;
         private static string _connectionString = "Server=ealdb1.eal.local; User ID=ejl20_usr; Password=Baz1nga20; Database=EJL20_DB";
 
-        public List<Model.Product> GetProducts() {
+        public List<Model.Product> GetAllProducts() {
             List<Model.Product> Products = new List<Model.Product>();
             try {
+        
+                SqlCommand cmd = new SqlCommand("GetAllProducts", dbconn);
+                cmd.CommandType = CommandType.StoredProcedure;
 
+                SqlDataReader rdr;
+                rdr = cmd.ExecuteReader();
+
+                while (rdr.HasRows && rdr.Read()) {
+                    int RecipeID = (int)rdr["RecipeID"];
+                    string Variant = (string)rdr["Variant"];
+                    int Size = (int)rdr["Size"];
+                    int FruitContent = (int)rdr["FruitContent"];
+                    double Price  = (double)rdr["Price"];
+                    Model.Product p = new Model.Product(RecipeID, Variant, Size, FruitContent, Price);
+                    Products.Add(p);
+                }
+
+                CloseDB();
 
                 return Products;
             }
-            catch (Exception) {
+            catch (Exception e) {
                 
-                throw;
+                throw new Exception("Error in getting all Products" + e.Message);
             }
         }
+
+        public List<Model.>
 
         private static void ConnectDB() {
             dbconn = new SqlConnection(_connectionString);
--- JamFactory/Model/Product.cs
+++ JamFactory/Model/Product.cs
@@ -6,12 +6,14 @@ using System.Threading.Tasks;
 
 namespace JamFactory.Model {
     class Product {
+        public int RecipeID { get; set; }
         public string Variant { get; set; }
         public int Size { get; set; }
         public int FruitContent { get; set; }
         public double Price { get; set; }
 
-        public Product(string variant, int size, int fruitContent, double price) {
+        public Product(int recipeid, string variant, int size, int fruitContent, double price) {
+            RecipeID = recipeid;
             Variant = variant;
             Size = size;
             FruitContent = fruitContent;
